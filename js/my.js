51f9e90375ef4513b00e92071bf654ba5830a0a0
bryanlohjy <bryanlohjy@hotmail.com>
2016-03-25 03:39:59 +1300

    grid1

---
+++ js/my.js
@@ -0,0 +1,23 @@
+// var h = 0;
+
+// function setVerticalHeight(){
+// 	$(".uk-vertical-align").each(
+// 		function () {
+// 			if(window.innerWidth > 768){
+// 				$(this).height(h);
+// 			}else{
+// 				$(this).css({"height" : "auto"});
+// 			}
+// 		}
+// 	);
+// }
+// $(document).ready(function(){
+// 	h = $(".uk-vertical-align").first().parent().height();
+// 	setVerticalHeight();
+// 	$(window).resize(function(){
+// 	setVerticalHeight();
+// });
+// }
+
+// );
+
